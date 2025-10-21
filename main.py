a65781351d6d85af6b1237063d8db707b381c191
Reza <rezapapi138418@gmail.com>
2025-10-21 11:30:19 +0330

    add uploadImage concept to project for upload file or image into a server

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from db import models
 from db.database import engine
 from routers import user, posts
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -13,3 +14,5 @@ models.Base.metadata.create_all(engine)
 
 app.include_router(user.router)
 app.include_router(posts.router)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
--- routers/posts.py
+++ routers/posts.py
@@ -1,8 +1,9 @@
-from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi import Depends, APIRouter, HTTPException, status, File, UploadFile
 from sqlalchemy.orm.session import Session
 from db.database import get_db
 from routers.schemas import PostDisplay, PostBase
-from db.db_post import create_post
+from db.db_post import create_post, get_all_posts
+import shutil, os
 
 router = APIRouter(prefix='/posts', tags=['posts'])
 
@@ -13,3 +14,22 @@ def bulid_post(request: PostBase, db: Session = Depends(get_db)):
     if not request.image_url_type in image_url_types:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, detail='Parameter image_url_type can only take a values relative or absolute')
     return create_post(db, request)
+
+@router.get('/posts')
+def get_posts(db: Session = Depends(get_db)):
+    return get_all_posts(db)
+
+@router.post('/upload')
+def upload_image(image: UploadFile = File(...)):
+    upload_dir = "images"
+    os.makedirs(upload_dir, exist_ok=True)
+
+    file_path = os.path.join(upload_dir, image.filename)
+    
+    with open(file_path, 'wb') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+    
+    return {
+        'image': image.filename,
+        'image_type': image.content_type
+    }
